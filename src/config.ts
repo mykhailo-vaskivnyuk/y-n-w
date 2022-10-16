280f645ffae85e44d2d33ad869542d59a48f8de8
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-10-16 13:45:41 +0300

    fix: heroku database

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export = {
   database: {
     queriesPath: buildPath + '/db/queries',
     connection: {
-      connectionString: process.env.DATABASE_URL || '',
+      connectionString: process.env.DATABASE_URL || ' ',
       ssl: {
         rejectUnauthorized: false,
       },
