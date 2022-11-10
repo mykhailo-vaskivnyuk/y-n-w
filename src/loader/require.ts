4fd8106fbc7a925f73863795021ce792bc86169f
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-10 09:17:10 +0200

    feat: require experiment

--- src/loader/require.ts
+++ src/loader/require.ts
@@ -5,7 +5,7 @@ import { TCache, TRequire } from './types';
 import { getScriptInContext, log, resolve } from './utils';
 
 const options = { displayErrors: true };
-const curDirName = __dirname;
+// const curDirName = __dirname;
 const cache: TCache = {};
 
 export const loadModule = (
@@ -16,6 +16,8 @@ export const loadModule = (
   const __dirname = require.main?.path || path.resolve('.');
   if (mode !== 'isolate_all') vm.createContext(context);
   const newRequire = getRequire(__dirname, context) as TRequire;
+  newRequire.cache = {};
+  newRequire.main = { path: __dirname };
   try {
     return newRequire(modulePath);
   } finally {
@@ -24,30 +26,35 @@ export const loadModule = (
 };
 
 export const getRequire = (
-  moduleDir: string,
+  parentModuleDir: string,
   context: vm.Context | IModulesContext,
-) => (modulePath: string) => {
-  const __filename = resolve(moduleDir, modulePath);
-  if (!__filename) return require(modulePath);
-  const __dirname = path.dirname(__filename);
-  if (__filename in cache) return cache[__filename];
-  log(__filename);
-  const scriptInContext = getScriptInContext(__filename);
-  const newRequire = getRequire(__dirname, context) as TRequire;
-  newRequire.main = { path: curDirName };
-  newRequire.cache = cache;
-  const module = { exports: {} };
-  if (!vm.isContext(context)) vm.createContext(context);
-  const contextParams = {
-    global: context,
-    require: newRequire,
-    module,
-    exports: module.exports,
-    __filename,
-    __dirname,
-  };
-  const wrapper = vm.runInContext(scriptInContext, context, options);
-  wrapper(contextParams);
-  cache[__filename] = module.exports;
-  return module.exports;
+) => {
+  const curRequire = ((modulePath: string) => {
+    const __filename = resolve(parentModuleDir, modulePath);
+    if (!__filename) return require(modulePath);
+    const __dirname = path.dirname(__filename);
+    if (__filename in (curRequire?.cache || {})) return cache[__filename];
+    log(__filename);
+    const scriptInContext = getScriptInContext(__filename);
+    const newContext = !vm.isContext(context);
+    const nextContext = newContext ? vm.createContext({ ...context }) : context;
+    console.log(newContext);
+    const newRequire = getRequire(__dirname, context) as TRequire;
+    newRequire.cache = newContext ? {} as TCache : curRequire.cache;
+    newRequire.main = { path: parentModuleDir };
+    const module = { exports: {} };
+    const contextParams = {
+      global: nextContext,
+      require: newRequire,
+      module,
+      exports: module.exports,
+      __filename,
+      __dirname,
+    };
+    const wrapper = vm.runInContext(scriptInContext, nextContext, options);
+    wrapper(contextParams);
+    newRequire.cache[__filename] = module.exports;
+    return module.exports;
+  }) as TRequire;
+  return curRequire;
 };
