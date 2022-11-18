af9c11ebc9ccb2251be7cd4f32538360935de692
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-18 08:56:00 +0200

    fix: setup db script

--- src/db/setup/setup.heroku.sh
+++ src/db/setup/setup.heroku.sh
@@ -3,6 +3,7 @@ echo 'start'
 export DATABASE=postgresql-regular-27985
 export APP=merega
 
+heroku pg:psql $DATABASE --app $APP -f create.heroku.sql
 heroku pg:psql $DATABASE --app $APP -f structure.sql
 # heroku pg:psql $DATABASE --app $APP -f data.sql
 
