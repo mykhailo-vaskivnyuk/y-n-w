40447cb4a92bf5b09bac2feaa3929e4b2170aabc
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-24 17:19:32 +0200

    feat: update front

--- src/db/setup/heroku/setup.heroku.sh
+++ src/db/setup/heroku/setup.heroku.sh
@@ -4,7 +4,7 @@ export DATABASE=postgresql-regular-27985
 export APP=merega
 
 heroku pg:psql $DATABASE --app $APP -f create.heroku.sql
-heroku pg:psql $DATABASE --app $APP -f structure.sql
+heroku pg:psql $DATABASE --app $APP -f ../structure.sql
 # heroku pg:psql $DATABASE --app $APP -f data.sql
 
 echo 'end'
