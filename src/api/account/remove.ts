5a16fe709da7fc6c5f419e81ff703c75ee7b10e6
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-25 13:42:43 +0200

    fix: leave net and user remove

--- src/api/account/remove.ts
+++ src/api/account/remove.ts
@@ -5,7 +5,8 @@ import { updateCountOfMemebers } from '../utils/utils';
 const remove: THandler = async ({ session }) => {
   const user_id = await session.read('user_id');
   await session.clear();
-  const nodes = await execQuery.net.nodes.removeUser([null, user_id!]);
+  const nodes = await execQuery.user.net.getNodes([user_id!, null]);
+  await execQuery.net.nodes.removeUser([null, user_id!]);
   await execQuery.user.remove([user_id!]);
   for (const node of nodes) await updateCountOfMemebers(node!, -1);
   return true;
--- src/db/queries/net/nodes/removeUser.ts
+++ src/db/queries/net/nodes/removeUser.ts
@@ -15,11 +15,15 @@ const removeUser = `
     SELECT nodes.node_id FROM nodes
     LEFT JOIN nets ON nodes.first_node_id = nets.node_id
     WHERE 
-      user_id = $2 AND (
-        ($1 + 1) NOTNULL AND
-        nets.net_level >= (SELECT net_level FROM nets WHERE net_id = $1)
-      ) OR (
-        ($1 + 1) ISNULL AND true
+      nodes.user_id = $2 AND (
+        (
+          ($1 + 1) NOTNULL AND (
+            nets.net_id = $1 OR
+            nets.net_level > (SELECT net_level FROM nets WHERE net_id = $1)
+          )
+        ) OR (
+          ($1 + 1) ISNULL AND true
+        )
       )
     )
 `;
