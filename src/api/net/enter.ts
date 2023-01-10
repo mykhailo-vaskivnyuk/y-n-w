ab2ace4c68ac1d656b597d99aa281f351a4b74d2
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2023-01-10 13:54:39 +0200

    fix: net enter api method

--- src/api/net/enter.ts
+++ src/api/net/enter.ts
@@ -2,12 +2,14 @@ import { THandler } from '../../router/types';
 import {
   INetEnterParams, INetResponse,
 } from '../../client/common/api/types/types';
+import { HandlerError } from '../../router/errors';
 import { NetResponseSchema, NetEnterParamsSchema } from '../schema/schema';
 
 const enter: THandler<INetEnterParams, INetResponse> =
   async ({ session }, { net_node_id }) => {
     const user_id = session.read('user_id')!;
     const [net] = await execQuery.user.net.read([user_id, net_node_id]);
+    if (!net) throw new HandlerError('NOT_FOUND');
     return net!;
   };
 enter.paramsSchema = NetEnterParamsSchema;
