31a05677c22f42f9e8ef7fd7916e5a69d3fe43b1
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2024-01-26 16:21:57 +0200

    feat: add heroku main

--- src/db/setup/heroku/setup.heroku.sh
+++ src/db/setup/heroku/setup.heroku.sh
@@ -2,6 +2,8 @@ echo 'start'
 
 export DATABASE=postgresql-asymmetrical-81951
 export APP=merega
+# export DATABASE=postgresql-animated-67025
+# export APP=younworld
 
 heroku pg:psql $DATABASE --app $APP -f create.heroku.sql
 heroku pg:psql $DATABASE --app $APP -f ../structure.sql
