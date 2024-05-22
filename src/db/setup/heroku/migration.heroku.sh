a23b1d9346db616c8ae055cff993e31cd8929c7a
mykhailo-vaskivnyuk <m.vaskivnyk@gmail.com>
2024-05-22 22:37:20 +0300

    fix: file name

---
+++ src/db/setup/heroku/migration.heroku.sh
@@ -0,0 +1,13 @@
+echo 'start'
+
+# prod
+export DATABASE=postgresql-asymmetrical-81951
+export APP=merega
+
+# dev
+# DATABASE=postgresql-animated-67025
+# APP=younworld
+
+heroku pg:psql $DATABASE --app $APP -f ../migrations/alter.events.sql
+
+echo 'end'
